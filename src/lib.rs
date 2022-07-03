623a7afadc8f85594e34d04fb39362bed6561541
John Adler <john.adler@protonmail.ch>
2022-07-03 18:51:19 -0400

    Add essential dependencies and initial setup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+//! Celestia Node REST API wrapper.
+//!
+//! Wraps the Celestia Node API: <https://docs.celestia.org/developers/node-api>.
+
+pub use reqwest;
+pub use serde::{Deserialize, Serialize};
+
+pub const DEFAULT_BASE_URL: &str = "http://localhost:26658";
